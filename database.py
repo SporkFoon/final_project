5dd087458f138f67e31333055e0d17d3f2d1aa77
Navin Bunthuphanich <navin.b@ku.th>
2023-12-18 04:59:19 +0700

    Rewriting all of the database and project_manage file

--- database.py
+++ database.py
@@ -1,103 +1,37 @@
-# try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
-
-import csv, os
-
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-# print(persons)
-
-# add in code for a Database class
-
-# add in code for a Table class
-
-# modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
-
-# modify the code in the Table class so that it supports the update operation where an entry's value associated with a key can be updated
+import csv
+import os
 
 class Database:
     def __init__(self):
         self.tables = {}
 
-    def load_table(self, table_name, file_path):
-        table = Table(file_path)
+    def add_table(self, table_name, table):
         self.tables[table_name] = table
 
-    def load_login_table(self, table_name, file_path):
-        login_table = LoginTable(file_path)
-        self.tables[table_name] = login_table
-
-    def save_table(self, table_name):
-        if table_name in self.tables:
-            self.tables[table_name].save_data()
-        else:
-            print(f"Table '{table_name}' not found.")
-
 class Table:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.data = self.load_data()
+    def __init__(self, csv_file_path):
+        self.file_path = csv_file_path
+        self.entries = self.read_csv()
 
-    def load_data(self):
-        data = []
+    def read_csv(self):
         with open(self.file_path) as f:
             rows = csv.DictReader(f)
-            for r in rows:
-                data.append(dict(r))
-        return data
+            return [dict(r) for r in rows]
 
-    def save_data(self):
+    def write_csv(self):
         with open(self.file_path, 'w', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=self.data[0].keys())
+            fieldnames = self.entries[0].keys()
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerows(self.data)
+            writer.writerows(self.entries)
 
     def insert_entry(self, entry):
-        self.data.append(entry)
+        self.entries.append(entry)
+        self.write_csv()
 
     def update_entry(self, entry_id, key, value):
-        for entry in self.data:
-            if entry.get('ID') == entry_id:
+        for entry in self.entries:
+            if entry['ID'] == entry_id:
                 entry[key] = value
+                self.write_csv()
                 break
-
-class LoginTable(Table):
-    # Inherits from Table, but can have additional functionality specific to login information
-    def authenticate(self, username, password):
-        for entry in self.data:
-            if entry['username'] == username and entry['password'] == password:
-                return entry['role']
-        return None
-
-# # Example usage
-# if __name__ == "__main__":
-#     # Instantiate the Database
-#     db = Database()
-
-#     # Load the 'persons' table
-#     db.load_table('persons', 'persons.csv')
-
-#     # Load the 'login' table
-#     db.load_login_table('login', 'login.csv')
-
-#     # Insert a new entry into the 'persons' table
-#     new_person = {'ID': '9999999', 'first': 'New', 'last': 'Person', 'type': 'student'}
-#     db.tables['persons'].insert_entry(new_person)
-
-#     # Update the 'type' of a specific entry in the 'persons' table
-#     db.tables['persons'].update_entry('9999999', 'type', 'lead')
-
-#     # Save changes to the 'persons' table
-#     db.save_table('persons')
-
-#     # Print the updated 'persons' table
-#     print(db.tables['persons'].data)
-
-#     # Authenticate a user
-#     role = db.tables['login'].authenticate('Cristiano.R', '2255')
-#     print(f"Authenticated user role: {role}")
--- project_manage.py
+++ project_manage.py
@@ -1,88 +1,233 @@
-# import database module
-from database import Database, Table, LoginTable
+from database import Database, Table
+import os
 
-# define a funcion called initializing
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-def initializing():
-
-# here are things to do in this function:
-
-    # create an object to read all csv files that will serve as a persistent state for this program
-
-    # create all the corresponding tables for those csv files
-
-    # see the guide how many tables are needed
-
-    # add all these tables to the database
-
-
-# define a funcion called login
-
-    db = Database()
-
-    db.load_table('persons', 'persons.csv')
-    db.load_login_table('login', 'login.csv')
-
-    db.save_table('persons')
-    db.save_table('login')
-
-def login():
-
-# here are things to do in this function:
-   # add code that performs a login task
-        # ask a user for a username and password
-        # returns [ID, role] if valid, otherwise returning None
-
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-
-    user_info = db.tables['login'].authenticate(username, password)
-    return user_info
-
-# define a function called exit
-def exit():
-
-# here are things to do in this function:
-   # write out all the tables that have been modified to the corresponding csv files
-   # By now, you know how to read in a csv file and transform it into a list of dictionaries. For this project, you also need to know how to do the reverse, i.e., writing out to a csv file given a list of dictionaries. See the link below for a tutorial on how to do this:
-   
-   # https://www.pythonforbeginners.com/basics/list-of-dictionaries-to-csv-in-python
-
-
-# make calls to the initializing and login functions defined above
-    db.save_table('persons')
-    db.save_table('login')
-
+# Create a Database object
 db = Database()
+
+# Define function to initialize
+# Define function to initialize
+def initializing():
+    global db
+
+    # Specify the correct paths to your CSV files
+    persons_csv_path = os.path.join(__location__, 'persons.csv')  # Replace with the correct filename
+    login_csv_path = os.path.join(__location__, 'login.csv')      # Replace with the correct filename
+
+    # Create tables and add them to the database
+    persons_table = Table(persons_csv_path)
+    login_table = Table(login_csv_path)
+
+    db.add_table('persons', persons_table)
+    db.add_table('login', login_table)
+
+
+# Define function to perform login task
+def login():
+    # Placeholder code for simplicity, implement actual login logic
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    # Placeholder logic, replace with actual login logic
+    for person in db.tables['login'].entries:
+        if person['username'] == username and person['password'] == password:
+            return [person['ID'], person['role']]
+    return None
+
+# Define function to exit and write modified tables to CSV files
+def exit():
+    global db
+
+    # Write modified tables to CSV files
+    for table_name, table in db.tables.items():
+        table.write_csv()
+
+# Define menu method for users to choose their desired actions
+def show_menu(role):
+    print("1. View and respond to invitations")
+    print("2. Initiate and lead a project")
+
+    if role == 'admin':
+        print("3. Manage advisee projects")
+        print("4. Review proposal approval requests")
+        print("5. Evaluate project reports")
+
+    elif role == 'student':
+        print("3. View and modify project details")
+        print("4. Leave a project group")
+
+    elif role == 'lead':
+        print("3. Send invitations to other students")
+        print("4. Invite faculty members to be advisors")
+        print("5. View and modify project details")
+        print("6. Submit project proposals for advisor approval")
+        print("7. Submit project reports for faculty member evaluation")
+        print("8. Approve member leave requests")
+
+    elif role == 'member':
+        print("3. View and modify project details")
+        print("4. Leave a project group")
+
+    elif role == 'faculty':
+        print("3. View and respond to advisor requests")
+        print("4. Evaluate project reports")
+
+    elif role == 'advisor':
+        print("3. View and modify advisee projects")
+        print("4. Review proposal approval requests")
+        print("5. Evaluate project reports")
+
+# Student class
+class Student:
+    def __init__(self, person_id, username):
+        self.person_id = person_id
+        self.username = username
+
+    def view_and_respond_invitations(self):
+        # Placeholder logic
+        print("Viewing and responding to invitations.")
+
+    def initiate_lead_project(self):
+        # Placeholder logic
+        print("Initiating and leading a project.")
+
+# LeadStudent class
+class LeadStudent(Student):
+    def __init__(self, person_id, username, project_name):
+        super().__init__(person_id, username)
+        self.project_name = project_name
+
+    def send_invitations(self):
+        # Placeholder logic
+        print("Sending invitations to other students.")
+
+    def invite_advisors(self):
+        # Placeholder logic
+        print("Inviting faculty members as advisors.")
+
+    def view_modify_project_details(self):
+        # Placeholder logic
+        print("Viewing and modifying project details.")
+
+    def submit_project_proposal(self):
+        # Placeholder logic
+        print("Submitting project proposal for advisor approval.")
+
+    def submit_project_report(self):
+        # Placeholder logic
+        print("Submitting project report for faculty member evaluation.")
+
+    def approve_member_leave_requests(self):
+        # Placeholder logic
+        print("Approving member leave requests.")
+
+# Member class
+class Member(Student):
+    def __init__(self, person_id, username, project_name):
+        super().__init__(person_id, username)
+        self.project_name = project_name
+
+    def view_modify_project_details(self):
+        # Placeholder logic
+        print("Viewing and modifying project details.")
+
+    def leave_project_group(self):
+        # Placeholder logic
+        print("Leaving a project group.")
+
+# FacultyMember class
+class FacultyMember:
+    def __init__(self, person_id, username):
+        self.person_id = person_id
+        self.username = username
+
+    def view_respond_advisor_requests(self):
+        # Placeholder logic
+        print("Viewing and responding to advisor requests.")
+
+    def evaluate_project_reports(self):
+        # Placeholder logic
+        print("Evaluating project reports.")
+
+# Advisor class
+class Advisor(FacultyMember):
+    def __init__(self, person_id, username, advisee_project):
+        super().__init__(person_id, username)
+        self.advisee_project = advisee_project
+
+    def view_modify_advisee_projects(self):
+        # Placeholder logic
+        print("Viewing and modifying advisee projects.")
+
+    def review_proposal_approval_requests(self):
+        # Placeholder logic
+        print("Reviewing proposal approval requests.")
+
+    def evaluate_project_reports(self):
+        # Placeholder logic
+        print("Evaluating project reports.")
+
+# Admin class
+class Admin(FacultyMember):
+    def __init__(self, person_id, username):
+        super().__init__(person_id, username)
+
+    def manage_advisee_projects(self):
+        # Placeholder logic
+        print("Managing advisee projects.")
+
+    def review_proposal_approval_requests(self):
+        # Placeholder logic
+        print("Reviewing proposal approval requests.")
+
+    def evaluate_project_reports(self):
+        # Placeholder logic
+        print("Evaluating project reports.")
+
+# Make calls to initializing and login functions
 initializing()
 val = login()
 
-# based on the return value for login, activate the code that performs activities according to the role defined for that person_id
+# Based on the return value for login, activate the code that performs activities according to the role
+if val:
+    show_menu(val[1])
 
-# if val[1] = 'admin':
-    # see and do admin related activities
-# elif val[1] = 'student':
-    # see and do student related activities
-# elif val[1] = 'member':
-    # see and do member related activities
-# elif val[1] = 'lead':
-    # see and do lead related activities
-# elif val[1] = 'faculty':
-    # see and do faculty related activities
-# elif val[1] = 'advisor':
-    # see and do advisor related activities
+    if val[1] == 'admin':
+        admin = Admin(val[0], val[1])
+        admin.manage_advisee_projects()
+        admin.review_proposal_approval_requests()
+        admin.evaluate_project_reports()
 
-# once everyhthing is done, make a call to the exit function
-if val and val[1] == 'admin':
-    pass
-elif val and val[1] == 'student':
-    pass
-elif val and val[1] == 'member':
-    pass
-elif val and val[1] == 'lead':
-    pass
-elif val and val[1] == 'faculty':
-    pass
-elif val and val[1] == 'advisor':
-    pass
+    elif val[1] == 'student':
+        student = Student(val[0], val[1])
+        student.view_and_respond_invitations()
+        student.initiate_lead_project()
+
+    elif val[1] == 'lead':
+        lead_student = LeadStudent(val[0], val[1], "ProjectX")
+        lead_student.send_invitations()
+        lead_student.invite_advisors()
+        lead_student.view_modify_project_details()
+        lead_student.submit_project_proposal()
+        lead_student.submit_project_report()
+        lead_student.approve_member_leave_requests()
+
+    elif val[1] == 'member':
+        member = Member(val[0], val[1], "ProjectX")
+        member.view_modify_project_details()
+        member.leave_project_group()
+
+    elif val[1] == 'faculty':
+        faculty_member = FacultyMember(val[0], val[1])
+        faculty_member.view_respond_advisor_requests()
+        faculty_member.evaluate_project_reports()
+
+    elif val[1] == 'advisor':
+        advisor = Advisor(val[0], val[1], "AdviseeProject")
+        advisor.view_modify_advisee_projects()
+        advisor.review_proposal_approval_requests()
+        advisor.evaluate_project_reports()
+
+# Once everything is done, make a call to the exit function
 exit()
